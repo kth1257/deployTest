0a95515d0e0eb02bd2bb46f74725d53c6207142a
taehun.kim <xognswldk@gmail.com>
2025-07-25 16:17:15 +0900

    배포연습

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+    return (
+        <>
+            <h1>This is Deploy TEST!!!</h1>
+            <h2>나는 이제 배포의 달인</h2>
+            Hello vite-React!
+        </>
+    )
+}
+
+export default App
